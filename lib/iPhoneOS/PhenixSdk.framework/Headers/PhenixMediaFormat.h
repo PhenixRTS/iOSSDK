f1c2071f5bdf776d9eb0dd18bd6023cc40ee756f
Yevgen Marchuk <yevgen.marchuk@phenixrts.com>
2019-01-09 19:17:02 +0200

    v2018.4.3 Features/Improvements - URIs passed to SDK can now accept leading and trailing whitespaces without causing failures - Improved chat message observable behavior to ensure that initial callback will always contain most recent chat history - Allow switching of camera resolution while publishing - Support for BGRA pixel format when using FrameReady API
    Fixes
    - Fixed detached functionality when publishing remote streams via express API
    - Fixed sporadic DTLS connection issues

---
+++ lib/iPhoneOS/PhenixSdk.framework/Headers/PhenixMediaFormat.h
@@ -0,0 +1,6 @@
+/**
+ * Copyright 2018 PhenixP2P Inc. Confidential and Proprietary. All Rights Reserved.
+ */
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, PhenixMediaFormat) { PhenixMediaFormatI420, PhenixMediaFormatNV12, PhenixMediaFormatBGRA };
